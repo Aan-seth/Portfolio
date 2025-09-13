7617abdc3308309e2322aaa8a77d95d9d6ef48ee
Aan-seth <sethaan604@gmail.com>
2025-09-13 12:34:12 +0530

    UI updated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 w-full py-4 bg-gray-800 text-center text-sm text-white z-50">
       <p>
-        © {new Date().getFullYear()} Simon Game | Built with ❤️ by{" "}
+        © {new Date().getFullYear()} Portfolio | Built with ❤️ by{" "}
         <a
           className="hover:text-pink-500 dark:hover:text-pink-400 hover:underline transition"
           href="https://github.com/Aan-seth"
